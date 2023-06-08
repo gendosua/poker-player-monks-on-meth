31f7a234ddcdcae301bf1a263793e4249d5029d3
Hennadii Shymanskyi <hennadii.shymanskyi@contentful.com>
2023-06-08 11:07:42 +0200

    chore: raise rand 250 to 500

--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,10 @@
 export class Player {
   public betRequest(gameState: any, betCallback: (bet: number) => void): void {
-    betCallback(250);
+    betCallback(this.generateRandomInteger(250, 500));
+  }
+
+  private generateRandomInteger(min: number, max: number): number {
+    return Math.floor(min + Math.random()*(max - min + 1))
   }
 
   public showdown(gameState: any): void {
